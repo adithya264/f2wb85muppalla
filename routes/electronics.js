0a2aec7cb16fa9fb1329132b35617e71156e6dfc
adithya264 <adithya369varaprasad@gmail.com>
2022-11-02 22:29:53 -0500

    added

--- routes/electronics.js
+++ routes/electronics.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    let query = req.query
-    rows = query.rows
-    cols = query.cols
-    console.log(`rows ${rows}`)
-    console.log(`cols ${cols}`)
-    res.render('gridbuild', {title: 'Grid Build', query: query});
+  res.render('electronics', { title: 'Search Results for electronics' });
 });
 
 module.exports = router;
